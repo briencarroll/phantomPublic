4ab198345794523d218c5648197684694797d5ac
briencarroll <brien.carroll2@gmail.com>
2017-09-15 00:55:49 -0400

    media query

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import Brick from './assets/brick.jpg';
 import EP from './assets/ep.jpg';
 import '../node_modules/reset-css/reset.css'
 import './styles/site.css'
-
 import './scripts/site.js'
-
-
 
 document.addEventListener("DOMContentLoaded", (e) => {
   const albumCover = document.querySelector('.me');
--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,1 +1,4 @@
 console.log('Hello, we are Phantom Public');
+console.log('Thanks for visiting. Our first EP, Into The Blue, has been released')
+console.log('We hope you enjoy it')
+console.log('And we hope that everyone stops killing one another')
