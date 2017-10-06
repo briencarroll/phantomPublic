2197f50d854d60d0cce81ee49763c31b936beabf
briencarroll <brien.carroll2@gmail.com>
2017-10-06 01:58:49 -0400

    lol console

--- bundle.js
+++ bundle.js
@@ -738,7 +738,7 @@ exports.push([module.i, "html {\n  box-sizing: border-box;\n}\n*, *:before, *:af
 
 console.error('Hello, we are Phantom Public');
 console.warn('Thanks for visiting. Our first EP, Into The Blue, has been released');
-console.table('We hope you enjoy it');
+console.info('We hope you enjoy it');
 console.error('be kind to living things');
 
 /***/ })
